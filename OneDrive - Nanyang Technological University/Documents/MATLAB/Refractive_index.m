bfaef7b36cf50552291b01db906393c8afc4bbd5
#WU YUJIN# <YUJIN003@student.main.ntu.edu.sg>
2025-05-21 15:27:02 +0800

    对Drude进行了进一步简化

--- OneDrive - Nanyang Technological University/Documents/MATLAB/Refractive_index.m
+++ OneDrive - Nanyang Technological University/Documents/MATLAB/Refractive_index.m
@@ -1,14 +1,31 @@
 clear;
-w=11.61;         %wInP=9.61or9.52, wInGaAs=11.61or11.57, wInAlAs=9.94or9.84
-N=8e18*1e6;     %doping [/m^3]
-wl=13e-6;     %wavelength[m]
-m=0.043;         %InP=0.073/0.079, InGaAs=0.043/0.04, InAlAs=0.072
-tau=100*1e-15;    %100fs
-omega=2*pi*(3e8)/wl;
 
-wp2=N*(1.602e-19)^2/(8.8542e-12)/w/(9.10938e-31*m);
-wp=sqrt(wp2);
-wl_wp=3e8/wp;
-re=abs(w*(1-wp2/(omega^2+(1/tau^2))));
-im=w*wp2*tau/(omega+omega^3*tau^2);
-n=sqrt(re+1i*im);
+% 1) Material parameters
+epsilon_infinity    = 9.61;                % high‐frequency permittivity
+N     = 1.7e18 * 1e6;           % [m⁻³] Doping concentration
+m_eff = 0.073 * 9.10938e-31;  % [kg] effective mass
+tau     = 100e-15;              % [s] scattering time
+gamma     = 1/tau;                  % [s⁻¹]
+
+% 2) Optical frequency
+lambda_0    = 7.3e-6;                % [m]
+omega     = 2*pi*3e8/lambda_0;          % [rad/s]
+
+% 3) Plasma frequency
+epsilon_0    = 8.8542e-12;           % [F/m]
+e     = 1.602e-19;            % [C]
+omega_p2   = N*e^2/(epsilon_0*m_eff);     % [rad²/s²]
+omega_p    = sqrt(omega_p2);
+
+% 4) Drude permittivity
+epsilon_drude = epsilon_infinity - omega_p2./(omega^2 + 1i*(1/tau)*omega);
+
+% 5) Refractive index
+n = sqrt(epsilon_drude);
+
+% 6) Optional: plasma‐wavelength (for reference)
+lambda_p = 2*pi*3e8/omega_p;
+fprintf('Plasma wavelength: %.2f µm\n', lambda_p*1e6);
+
+% Show real and imag parts
+fprintf('n = %.3f + %.3fi\n', real(n), imag(n));
